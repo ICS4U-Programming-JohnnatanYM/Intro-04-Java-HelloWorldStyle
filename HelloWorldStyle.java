f1c301a6899b64dac2ff4ab232175913ac8b5296
Johnnatan Yasin Medina <johnnatan.yasin-medina@stu.ocsb.ca>
2025-02-11 00:55:01 +0000

    Added code and used linter

---
+++ HelloWorldStyle.java
@@ -0,0 +1,26 @@
+/**
+*
+* This program simply displays a greetings message to my teacher.
+*
+* @author  Johnnatan Yasin Medina
+* @version 1.0
+* @since   2025-02-10
+
+*/
+public final class HelloWorldStyle {
+
+    /**
+     * @exception IllegalStateException
+     * @see IllegalStateException
+    */
+
+    private HelloWorldStyle() {
+        throw new IllegalStateException("Utility Clean");
+    }
+     /**
+      * @param args
+      */
+    public static void main(final String[] args) {
+        System.out.println("Hello Ms Raffin!");
+    }
+}
